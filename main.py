2c22e6c2959b9cd3057f0ba7d6899d09bdc4c487
ajay <yajay46394@gmail.com>
2022-11-23 10:02:46 +0530

    learning python basics

---
+++ main.py
@@ -0,0 +1,69 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from math import *
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # for i in range(2, 20, 3):
+    #     print(i)
+    #
+
+    # dealing with strings
+
+    # name = "ajaY_Yadav"
+    # print(name[2:])
+    # print(name[:len(name) - 1])
+    #
+    # for i in range(0, len(name)-1, 1):
+    #     print(name.replace("jaY", "jubbi"))
+
+    # dealing with numbers
+
+    # print(5 * 3)
+    # print(str(5 * 3 / 5) + str(True) + str("ajay"))
+    # print(abs(-10))
+    # print(max(20, 30))
+    # print(round(2.9))
+    # print(round(2.1))
+    # print(floor(2.9))
+    # print(ceil(2.1))
+    # print(sqrt(36))
+
+
+    #taking input from the user
+
+    # person = input("what s your name ?")
+    # print("hello "+ person+ "  !!")
+
+    #building a calculator
+
+    #lists in python
+    # people = ["ajay", "shrini", "ballu", 2, False]
+    # for p in people:
+    #     print(p)
+    #
+    # for i in range(0,4,1):
+    #     print(people[i])
+
+    #we can also use -ve indexing in list in python, the backward indexing starts from -1
+    # print(people[-2])
+
+    #accessing all the elements after some index
+    # print(people[1:])
+
+    #accessing some elements in a range not including the last position
+    # print(people[1:3])
+
+    #changing values at some index
+    # people[4] = "brumma"
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
